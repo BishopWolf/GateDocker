efd115289ed6b98e78bdee981fe13ec179045247
Alex Vergara Gil <alexvergaragil@gmail.com>
2021-02-10 15:06:55 +0100

    fix bug

--- Generate.sh
+++ Generate.sh
@@ -2,7 +2,7 @@
 ROOT_Version=v6-19-02
 Geant4_Version=v10.6.1
 Geant4_Tag=bishopwolf/geant4:10.6.1
-Gate_Version=9.0
+Gate_Version=v9.0
 Gate_Tag=bishopwolf/gate:9.0
 
 docker build -t $Geant4_Tag -f DockerFileGeant --build-arg ROOT_Version=$ROOT_Version --build-arg Geant4_Version=$Geant4_Version .
