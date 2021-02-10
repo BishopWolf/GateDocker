baa819ba1098e59f0abc6b2459bf576cac52ba21
Alex Vergara Gil <alexvergaragil@gmail.com>
2021-02-10 15:27:10 +0100

    fix bug

--- Generate.sh
+++ Generate.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-ROOT_Version=v6-19-02
-Geant4_Version=v10.6.1
-Geant4_Tag=bishopwolf/geant4:10.6.1
-Gate_Version=v9.0
-Gate_Tag=bishopwolf/gate:9.0
 
-docker build -t $Geant4_Tag -f DockerFileGeant --build-arg ROOT_Version=$ROOT_Version --build-arg Geant4_Version=$Geant4_Version .
+#Variables to modify
+Repository=bishopwolf
+ROOT_Version=v6-19-02
+Geant4_Version=10.6.1
+Gate_Version=9.0
+
+#Variables to preserve
+Geant4_Tag=$Repository/geant4:$Geant4_Version
+Gate_Tag=$Repository/gate:$Gate_Version
+
+docker build -t $Geant4_Tag -f DockerFileGeant --build-arg ROOT_Version=$ROOT_Version --build-arg Geant4_Version=v$Geant4_Version .
 docker push $Geant4_Tag
 
-docker build -t $Gate_Tag -f DockerFileGate --build-arg Geant4_Version=$Geant4_Tag --build-arg Gate_Version=$Gate_Version .
+docker build -t $Gate_Tag -f DockerFileGate --build-arg Geant4_Version=$Geant4_Tag --build-arg Gate_Version=v$Gate_Version .
 docker push $Gate_Tag
